f610a2c4849b25f371686e6286ed86376c4a57bd
Ciaran Eddy <eddyciaran@gmail.com>
2025-02-09 18:28:21 +0000

    Switch home page to using getServerSession()

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-"use client";
-
 import Image from "next/image";
 import Link from "next/link";
-import { useSession } from "next-auth/react";
+import { getServerSession } from "next-auth";
 
-export default function Home() {
-  const { data: session } = useSession();
+export default async function Home() {
+  const session = await getServerSession();
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
